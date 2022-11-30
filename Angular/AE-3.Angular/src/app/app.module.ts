c5e4589246ea9c37733db6c5ee4faa53969c43d0
Salvador.Estorch <salvadorestorch@gmail.com>
2022-11-30 17:02:59 +0100

    update

--- Angular/AE-3.Angular/src/app/app.module.ts
+++ Angular/AE-3.Angular/src/app/app.module.ts
@@ -3,16 +3,17 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ListadoVideojuegosComponent } from './componentes/listado-videojuegos/listado-videojuegos.component';
+import { DetalleVideojuegoComponent } from './componentes/detalle-videojuego/detalle-videojuego.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ListadoVideojuegosComponent,
+    DetalleVideojuegoComponent,
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  imports: [BrowserModule, AppRoutingModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
